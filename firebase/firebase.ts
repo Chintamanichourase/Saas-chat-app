4ba56b7452667eb81ccf70587a92366bf15f2e4d
Chintamani Chourase <chintamanichourase@gmail.com>
2026-01-03 02:02:08 +0530

    gitignore fix again

---
+++ firebase/firebase.ts
@@ -0,0 +1,21 @@
+import{getApp, getApps, initializeApp} from "firebase/app"
+import {getAuth} from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getFunctions }  from "firebase/functions"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCel0WSXyCJObaHhztM_OfaMCpUW9nPMK4",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_FOR_FIREBASETS,
+  storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const app=getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const db=getFirestore(app);
+const functions=getFunctions(app);
+
+export {db, auth, functions};  
